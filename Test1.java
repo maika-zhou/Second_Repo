56dbe2ebf56071fa9a275e351d4fdddff3087a6e
maika <zhouxinwei333@163.com>
2020-01-30 22:23:50 +0800

    Commit 2 files

---
+++ Test1.java
@@ -0,0 +1,12 @@
+public class Test1
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello GIT");
+		System.out.println("Hello Maika");
+		System.out.println("Hello Alreen");
+		System.out.println("Hello Nathan");
+	}
+
+}
